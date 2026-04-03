611377c56fb6bc824af44f9d29a78f2c0590f6d9
shivamftp65 <shivamftp65@gmail.com>
2026-04-03 14:19:00 +0530

    Added email and password validator

---
+++ src/middlewares/auth-request-validators.js
@@ -0,0 +1,14 @@
+const validateUserAuth = (req, res, next) => {
+    if(!req.body.email || !req.body.password){
+        return res.status(400).json({
+            data: {},
+            success: false,
+            message: "Something went wrong",
+            err: 'Email or password missing in the auth request'
+        })
+    }
+}
+
+module.exports = {
+    validateUserAuth
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const UserController = require('../../controllers/user-controller');
-
+const { AuthRequestValidator } = require('../../middlewares/index');
 const router = express.Router();
 
-router.post("/signup", UserController.create);
-router.post("/signin", UserController.signIn);
+router.post("/signup", AuthRequestValidator.validateUserAuth ,UserController.create);
+router.post("/signin", AuthRequestValidator.validateUserAuth ,UserController.signIn);
 
 module.exports = router;
